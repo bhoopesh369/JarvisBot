f084d0f378167e2b9f4f5be7a2ec307ec5641263
Titan3699 <82646284+Titan3699@users.noreply.github.com>
2021-11-13 19:44:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
-
 import telebot
 import yfinance as yf
+from PIL import Image
+import requests
+from io import BytesIO
+import random
+import praw
 
 TOKEN = "2089092326:AAEmGJZwscquf99EMpgQuY89omPfqHMacks"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['Greet'])
-def greet(message):
-  bot.reply_to(message, "Hey! Hows it going?")
+Image_Link="https://source.unsplash.com/random/300x200?sig=${Math.random()}.jpg"
 
-@bot.message_handler(commands=['hello'])
+
+reddit = praw.Reddit(client_id="VKeB6HMwelofNSqZU1EgjQ",
+                     client_secret="uuGNN3YwmqgSpUk38BYChpum_L6q7A",
+                     username="Historical_Cash_5628",
+                     password="bjioknmlp",  
+                     user_agent="this is useless")
+
+def get_post(rddt, sbrddt, first_x_posts=100):
+    subreddit = rddt.subreddit(sbrddt)  # Get the subreddit
+    hot = subreddit.hot(limit=first_x_posts)  # Get the first 100 posts
+    post = random.choice(list(hot))  # Get a random post from them
+    return post
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  print(message.text)
+
+@bot.message_handler(commands=['greet','hello','hi'])
 def hello(message):
-  bot.send_message(message.chat.id, "Hello!")
+  bot.send_message(message.chat.id, "Hey! Hows it going?")
+
+@bot.message_handler(commands=['image','img'])
+def image(message):
+  img = open("cat.jpg", "rb")
+  bot.send_photo(message.chat.id, img)
+
+@bot.message_handler(commands=["randimg",'imgnet'])
+def imagenet(message):
+  response= requests.get(Image_Link)
+  imgnet= Image.open(BytesIO(response.content))
+  bot.send_photo(message.chat.id, imgnet)
+
+@bot.message_handler(commands=['vid','video'])
+def video(message):
+  vid= open("GPNAKKU.mp4", "rb")
+  bot.send_video(message.chat.id,vid)
 
 @bot.message_handler(commands=['wsb'])
 def get_stocks(message):
   response = ""
-  stocks = ['gme', 'amzn', 'TTM','nok']
+  stocks = ['gme', 'amzn','TTM','nok']
   stock_data = []
   for stock in stocks:
     data = yf.download(tickers=stock, period='2d', interval='1d')
@@ -59,5 +95,18 @@ def send_price(message):
   else:
     bot.send_message(message.chat.id, "No Data!?")
   pass
+
+
+@bot.message_handler(commands=["reddit", "r"])
+def send_post(message):
+    post = get_post(rddt=reddit, sbrddt="askreddit")
+    try:
+        response = requests.get(post.url)
+        img = Image.open(BytesIO(response.content))
+        bot.send_photo(message.chat.id, img, caption=post.title)
+    except Exception as e:
+        if "cannot identify image file <_io.BytesIO object at" in str(e):
+         bot.send_message(message.chat.id, f"{post.url}\n{post.title}")
+
 
 bot.polling() 
